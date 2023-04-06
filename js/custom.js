a12c503df73708adfc6205f1516d93379787d9d9
lisacommander <lisa@omnicommander.com>
2023-04-06 01:22:20 -0400

    Modified carousel

--- js/custom.js
+++ js/custom.js
@@ -63,8 +63,8 @@ jQuery(document).ready(function ($) {
     items:2,
     loop: true,
     margin: 10,
-    autoplay: false,
-    autoplayTimeout: 5000,
+    autoplay: true,
+    autoplayTimeout: 4000,
     responsiveClass: true,
     onInitialized:function(){
     },
